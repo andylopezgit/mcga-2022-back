e60afa9ebcb949182af21f025f83e9605dbfe2eb
andylopezgit <alopezciotta@gmail.com>
2022-09-27 20:30:58 -0300

    fix vercel.json

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 //const dotenv = require("dotenv").config();
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const express= require('express');
 const router = require("./routes")
 
