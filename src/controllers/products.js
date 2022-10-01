3c43f737a4c79559186aed641fc419a8d57a9514
andylopezgit <alopezciotta@gmail.com>
2022-10-01 18:06:42 -0300

    se implementa logica para controladores de  modelo producto

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -19,11 +19,25 @@ const create = (req, res) => {
     .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
 };
 
-const getProductById = (req, res) => {};
+const getProductById = (req, res) => {
+  const id = req.params.id;
+  Products.findOne({ _id: id })
+    .then((data) => res.json({ msj: "Product found", status: "200", data }))
+    .catch((error) => res.status(500).json({ msg: `Error: ${error}` }));
+};
 
-const deleteProductById = (req, res) => {};
+const deleteProductById = (req, res) => {
+  const id = req.params.id;
+  Products.deleteOne({ _id: id }).then().catch();
+};
 
-const updateProduct = (req, res) => {};
+const updateProduct = (req, res) => {
+  const id = req.params.id;
+  const product = req.body;
+  Products.findOneAndUpdate({ _id: id }, { ...product })
+    .then()
+    .catch();
+};
 
 module.exports = {
   getAll,
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,8 +3,8 @@ const products = require("../controllers/products");
 
 router.get("/", products.getAll);
 router.post('/"', products.create);
-router.get("/:id", products.getProductById);
-router.delete("/:id", products.deleteProductById);
-router.put("/id", products.updateProduct);
+router.get("/showone/:id", products.getProductById);
+router.delete("/delete/:id", products.deleteProductById);
+router.put("/update/id", products.updateProduct);
 
 module.exports = router;
