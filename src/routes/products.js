9a52878b50d817f3c9d36f6a361bf2d808cf14eb
ejecutorHarlan <54726387+ejecutorHarlan@users.noreply.github.com>
2022-12-10 19:09:56 -0300

    Feature Token Added to "/products" route

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,13 @@
-const router = require("express").Router();
-const products = require("../controllers/products");
-const { validateString } = require("../validators/validators");
+const router = require('express').Router();
+const products = require('../controllers/products');
+const { validateString } = require('../validators/validators');
 
-router.get("/", products.getAll);
-router.post("/", validateString, products.create);
-router.get("/showone/:id", products.getProductById);
-router.delete("/delete/:id", products.deleteProductById);
-router.put("/update/:id", products.updateProduct);
+router.get('/', products.getAll);
+router.get('/public', products.getAll);
+
+router.post('/', validateString, products.create);
+router.get('/showone/:id', products.getProductById);
+router.delete('/delete/:id', products.deleteProductById);
+router.put('/update/:id', products.updateProduct);
 
 module.exports = router;
