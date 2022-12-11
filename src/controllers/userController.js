a1eb63c741e5e77596999c2e58c219e69935c78d
andylopezgit <alopezciotta@gmail.com>
2022-12-11 18:08:45 -0300

    create function to handle check dates login from api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,19 @@ const User = require("../models/user");
 const Products = require("../models/products");
 
 const getUserById = (req, res) => {
-  const name = req.body.name;
+  const name = req.body.user;
   const pass = req.body.pass;
+  console.log(req.body);
 
   User.find({ name: name, pass: pass }, function (err, user) {
     if (err) {
       res.send({ msg: "user not found", err, isLogged: false });
     }
-    console.log(user);
-    res.json({ msg: "Login correct", user: user, isLogged: true });
+    if (user.length > 0) {
+      res.json({ msg: "Login correct", user: user, isLogged: true });
+    } else {
+      res.json({ msg: "Login correct", user: user, isLogged: false });
+    }
   });
 };
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productsRoutes = require("./products");
+const usersRoutes = require("./users");
 const pingController = require("../controllers/ping");
-const userController = require("../controllers/userController");
 const checkToken = require("../middlewares/token-check");
 
 router.use("/api/ping", pingController.responsePing);
 router.use("/api/products/public", productsRoutes);
 router.use("/api/products", checkToken, productsRoutes);
-router.use("/api/users", userController.getUserById);
+router.use("/api/users", usersRoutes);
 
 module.exports = router;
